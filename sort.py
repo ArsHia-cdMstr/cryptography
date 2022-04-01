13596e01382293f3fd98852c3bad77b49f7c7a87
arshia.mkh <arshia.mokhlesi@gmail.com>
2022-04-01 15:40:00 +0430

    write bubble sort code to complete part 4 in the project

--- sort.py
+++ sort.py
@@ -1,3 +1,5 @@
+
+# part 3
 def sort( arr ):
     
     comparingNum = 0 ;
@@ -16,3 +18,22 @@ def sort( arr ):
             comparingNum += 1
     
     return arr 
+
+#part 4
+def bubbleSort( arr ):
+    for i in range (len(arr)):
+
+        swapped = False 
+
+        for j in range (0 , len (arr) - i - 1):
+            
+            if arr [j] > arr [j+1] :
+
+                temp = arr [j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+
+                swapped = True 
+            
+        if not swapped :
+            break

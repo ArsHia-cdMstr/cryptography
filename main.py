82abf2cc7ccee4dc6b44538ac8f7ff581b105f30
mohammd oshkooh <m957070152@gmail.com>
2022-04-09 02:34:03 +0430

    Added UI

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow, QPushButton, QMessageBox
+
 from IntToChar import intToChar
 from MapCharToInt import MapCharToInt
 from part2 import section2
 from part5 import AbsoluteValue
 from placement import placement
 from sort import sort, bubbleSort
-from ui import ui
 
 
 def processing(filename):
@@ -37,5 +40,43 @@ def processing(filename):
     return g
 
 
+def dialog():
+    file, check = QFileDialog.getOpenFileName(None, "selection file", "", "Text Files (*.txt)")
+    result = processing(file)
+    return result
+
+
+def ui():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setFixedHeight(438)
+    win.setFixedWidth(960)
+    win.setStyleSheet("background-image : url(image.jpg)")
+    win.setWindowTitle("Cryptography")
+
+    button = QPushButton(win)
+    button.setFixedWidth(200)
+    button.setFixedHeight(50)
+    button.setStyleSheet("background-image : url(select.png)")
+    button.move((960 - 200) / 2, (432 - 50) / 2)
+
+    def on_button_clicked():
+        result = dialog()
+
+        alert = QMessageBox()
+        alert.setText("Encryption was successful\n"
+                      "-------------------------\n\n" + result)
+        alert.setWindowTitle("hmmmm")
+        alert.exec()
+
+    button.clicked.connect(on_button_clicked)
+
+    win.show()
+
+    sys.exit(app.exec_())
+
+
 if __name__ == '__main__':
+
     ui()
+
